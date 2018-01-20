f996a446776152113c03bd3f88e04b64e55694d5
Nicholas Dujay <nickdujay@gmail.com>
2018-01-20 15:41:00 -0500

    create new game

--- src/main.rs
+++ src/main.rs
@@ -5,20 +5,50 @@ extern crate rocket;
 extern crate rocket_contrib;
 extern crate serde;
 #[macro_use] extern crate serde_derive;
+extern crate uuid;
 extern crate ws;
 
+use rocket::State;
 use rocket_contrib::Json;
+use std::collections::HashMap;
+use std::sync::RwLock;
 use std::thread;
+use uuid::Uuid;
 use ws::listen;
+
+#[derive(Debug)]
+struct GameState;
+
+#[derive(Debug)]
+struct GamesState {
+  games: RwLock<HashMap<Uuid, GameState>>
+}
+
+impl GamesState {
+  fn new() -> GamesState {
+    GamesState { games: RwLock::new(HashMap::new()) }
+  }
+
+  fn create_new_game(&self) -> Uuid {
+    let uuid = Uuid::new_v4();
+    let state = GameState;
+    {
+      let mut w = self.games.write().unwrap();
+      w.insert(uuid, state);
+    }
+    uuid
+  }
+}
 
 #[derive(Debug, Serialize)]
 struct CreateGameResponse {
-  id: String
+  id: Uuid
 }
 
 #[post("/games")]
-fn create_game() -> Json<CreateGameResponse> {
-  Json(CreateGameResponse { id: "hello".to_owned() })
+fn create_game(games_list: State<GamesState>) -> Json<CreateGameResponse> {
+  let uuid = games_list.create_new_game();
+  Json(CreateGameResponse { id: uuid })
 }
 
 fn main() {
@@ -31,5 +61,8 @@ fn main() {
     }).unwrap();
   });
 
-  rocket::ignite().mount("/api", routes![create_game]).launch();
+  rocket::ignite()
+    .mount("/api", routes![create_game])
+    .manage(GamesState::new())
+    .launch();
 }
