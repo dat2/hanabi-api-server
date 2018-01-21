2e2e98c1fb7dde438c1d44185151c7f5cac36828
Nicholas Dujay <nickdujay@gmail.com>
2018-01-21 12:19:22 -0500

    add set name api

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,27 @@
 
 extern crate rocket;
 extern crate rocket_contrib;
+extern crate rocket_cors;
 extern crate serde;
 #[macro_use]
 extern crate serde_derive;
 extern crate uuid;
 
-use rocket::{Request, Response, State};
-use rocket::fairing::{Fairing, Info, Kind};
-use rocket::http::{ContentType, Header, Method};
+use rocket::State;
+use rocket::http::{Cookie, Cookies, Method};
+use rocket::response::status::NoContent;
 use rocket_contrib::Json;
+use rocket_cors::{AllowedOrigins, AllowedHeaders};
 use std::collections::HashMap;
 use std::env;
-use std::io::Cursor;
 use std::sync::RwLock;
 use uuid::Uuid;
 
+/* State */
 #[derive(Debug)]
-struct GameState;
+struct GameState {
+  creator: String
+}
 
 #[derive(Debug)]
 struct GamesState {
@@ -33,9 +37,9 @@ impl GamesState {
     }
   }
 
-  fn create_new_game(&self) -> Uuid {
+  fn create_new_game(&self, creator: String) -> Uuid {
     let uuid = Uuid::new_v4();
-    let state = GameState;
+    let state = GameState { creator: creator };
     {
       let mut w = self.games.write().unwrap();
       w.insert(uuid, state);
@@ -44,62 +48,48 @@ impl GamesState {
   }
 }
 
+/* api */
 #[derive(Debug, Serialize)]
 struct CreateGameResponse {
   id: Uuid,
 }
 
 #[post("/games")]
-fn create_game(games_list: State<GamesState>) -> Json<CreateGameResponse> {
-  let uuid = games_list.create_new_game();
+fn create_game(games_list: State<GamesState>, cookies: Cookies) -> Json<CreateGameResponse> {
+  let creator = cookies.get("name")
+    .map(|cookie| cookie.value())
+    .unwrap_or("anonymous");
+  let uuid = games_list.create_new_game(creator.to_owned());
   Json(CreateGameResponse { id: uuid })
 }
 
-struct Cors {
-  origin: String,
+#[derive(Debug, Deserialize)]
+struct SetNameRequest {
+  name: String
 }
 
-impl Cors {
-  fn new(origin: String) -> Cors {
-    Cors { origin: origin }
-  }
-}
-
-impl Fairing for Cors {
-  fn info(&self) -> Info {
-    Info {
-      name: "Add Cors headers to requests",
-      kind: Kind::Response,
-    }
-  }
-
-  fn on_response(&self, request: &Request, response: &mut Response) {
-    if request.method() == Method::Options || response.content_type() == Some(ContentType::JSON) {
-      response.set_header(Header::new(
-        "Access-Control-Allow-Origin",
-        self.origin.clone(),
-      ));
-      response.set_header(Header::new(
-        "Access-Control-Allow-Methods",
-        "POST, GET, OPTIONS",
-      ));
-      response.set_header(Header::new("Access-Control-Allow-Headers", "Content-Type"));
-      response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
-    }
-
-    if request.method() == Method::Options {
-      response.set_header(ContentType::Plain);
-      response.set_sized_body(Cursor::new(""));
-    }
-  }
+#[post("/name", format = "application/json", data = "<set_name>")]
+fn set_name(set_name: Json<SetNameRequest>, mut cookies: Cookies) -> NoContent {
+  cookies.add(Cookie::new("name", set_name.name.clone()));
+  NoContent
 }
 
 fn main() {
-  let cors_origin = env::var("CLIENT_SERVER_ORIGIN").unwrap_or("http://localhost:3000".to_string());
+  let client_server_origin = env::var("CLIENT_SERVER_ORIGIN").unwrap_or("http://localhost:3000".to_string());
+
+  let (allowed_origins, _) = AllowedOrigins::some(&[&client_server_origin]);
+  let fairing = rocket_cors::Cors {
+    allowed_origins: allowed_origins,
+    allowed_methods: vec![Method::Get, Method::Post, Method::Options].into_iter().map(From::from).collect(),
+    allowed_headers: AllowedHeaders::some(&["Content-Type"]),
+    allow_credentials: true,
+    ..Default::default()
+  };
+
 
   rocket::ignite()
-    .attach(Cors::new(cors_origin))
-    .mount("/api", routes![create_game])
+    .attach(fairing)
+    .mount("/api", routes![set_name, create_game])
     .manage(GamesState::new())
     .launch();
 }
