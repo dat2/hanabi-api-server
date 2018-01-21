3b022f2e13b26b3f2629a9bd1149c2d0aa519d2e
Nicholas Dujay <nickdujay@gmail.com>
2018-01-21 10:44:08 -0500

    add cors; remove ws

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,16 @@ extern crate rocket_contrib;
 extern crate serde;
 #[macro_use] extern crate serde_derive;
 extern crate uuid;
-extern crate ws;
 
-use rocket::State;
+use rocket::{Request, Response, State};
+use rocket::fairing::{Fairing, Info, Kind};
+use rocket::http::{Header, ContentType, Method};
 use rocket_contrib::Json;
 use std::collections::HashMap;
+use std::env;
+use std::io::Cursor;
 use std::sync::RwLock;
-use std::thread;
 use uuid::Uuid;
-use ws::listen;
 
 #[derive(Debug)]
 struct GameState;
@@ -51,17 +52,46 @@ fn create_game(games_list: State<GamesState>) -> Json<CreateGameResponse> {
   Json(CreateGameResponse { id: uuid })
 }
 
+struct Cors {
+  origin: String
+}
+
+impl Cors {
+  fn new(origin: String) -> Cors {
+    Cors {
+      origin: origin
+    }
+  }
+}
+
+impl Fairing for Cors {
+  fn info(&self) -> Info {
+    Info {
+      name: "Add Cors headers to requests",
+      kind: Kind::Response
+    }
+  }
+
+  fn on_response(&self, request: &Request, response: &mut Response) {
+    if request.method() == Method::Options || response.content_type() == Some(ContentType::JSON) {
+      response.set_header(Header::new("Access-Control-Allow-Origin", self.origin.clone()));
+      response.set_header(Header::new("Access-Control-Allow-Methods", "POST, GET, OPTIONS"));
+      response.set_header(Header::new("Access-Control-Allow-Headers", "Content-Type"));
+      response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
+
+    if request.method() == Method::Options {
+      response.set_header(ContentType::Plain);
+      response.set_sized_body(Cursor::new(""));
+    }
+  }
+}
+
 fn main() {
-  thread::spawn(|| {
-    listen("127.0.0.1:5000", |out| {
-      move |msg| {
-        println!("{:?}", msg);
-        out.send(msg)
-      }
-    }).unwrap();
-  });
+  let cors_origin = env::var("CLIENT_SERVER_URL").unwrap_or("http://localhost:3000".to_string());
 
   rocket::ignite()
+    .attach(Cors::new(cors_origin))
     .mount("/api", routes![create_game])
     .manage(GamesState::new())
     .launch();
